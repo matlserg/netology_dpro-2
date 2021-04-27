7f73769aad56b895289e65d7e25d721bd09303c7
matlserg <smatlin@i-kompas.ru>
2021-04-27 13:05:28 +0300

    Добавлено решение задания № 2 (+ output)

--- 2_spark_sql/ht_2.py
+++ 2_spark_sql/ht_2.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import sum, col, desc
+from pyspark.sql.functions import sum, col, desc, max
 
 spark = SparkSession \
     .builder \
@@ -10,9 +10,30 @@ csv_df = spark.read.option('header', True).option('inferSchema', True).csv('owid
 
 df_select = csv_df.filter((csv_df['date'] <= '2021-03-31') & (csv_df['date'] >= '2021-03-25'))\
             .filter(~csv_df.iso_code.contains('OWID'))\
-            .select('iso_code', 'location', 'new_cases')
+            .select('date', 'location', 'new_cases')
 
-df = df_select.groupBy('iso_code', 'location').agg(sum("new_cases").alias('sum_new_cases'))\
-    .orderBy(col('sum_new_cases').desc())
+df_groupby = df_select.groupBy('location').agg(max("new_cases").alias('max_new_cases'))
+
+df = df_groupby.join(df_select, on='location', how='left')\
+            .filter(col('max_new_cases') == col('new_cases'))\
+            .select('date', 'location', 'new_cases')\
+            .orderBy(col('new_cases').desc())
 
 df.show(10)
+
+"""
++----------+-------------+---------+
+|      date|     location|new_cases|
++----------+-------------+---------+
+|2021-03-25|       Brazil| 100158.0|
+|2021-03-26|United States|  77321.0|
+|2021-03-31|        India|  72330.0|
+|2021-03-31|       France|  59054.0|
+|2021-03-31|       Turkey|  39302.0|
+|2021-03-26|       Poland|  35145.0|
+|2021-03-31|      Germany|  25014.0|
+|2021-03-26|        Italy|  24076.0|
+|2021-03-25|         Peru|  19206.0|
+|2021-03-26|      Ukraine|  18226.0|
++----------+-------------+---------+
+"""
