75cd78b73635b3f21074170315f4b1d0406a77f7
matlserg <smatlin@i-kompas.ru>
2021-04-27 12:12:21 +0300

    Добавлено решение задания № 1 (+ output)

--- 2_spark_sql/ht_1.py
+++ 2_spark_sql/ht_1.py
@@ -13,3 +13,25 @@ df = csv_df.filter(csv_df['date'] == '2021-03-31')\
                     round(col('total_cases_per_million') / 10_000, 2).alias('recovered_percent '))\
             .orderBy(col('total_cases_per_million').desc())
 df.show(15)
+
+"""
++--------+-------------+------------------+
+|iso_code|     location|recovered_percent |
++--------+-------------+------------------+
+|     AND|      Andorra|             15.54|
+|     MNE|   Montenegro|             14.52|
+|     CZE|      Czechia|             14.31|
+|     SMR|   San Marino|             13.94|
+|     SVN|     Slovenia|             10.37|
+|     LUX|   Luxembourg|              9.85|
+|     ISR|       Israel|              9.63|
+|     USA|United States|               9.2|
+|     SRB|       Serbia|              8.83|
+|     BHR|      Bahrain|              8.49|
+|     PAN|       Panama|              8.23|
+|     PRT|     Portugal|              8.06|
+|     EST|      Estonia|              8.02|
+|     SWE|       Sweden|              7.97|
+|     LTU|    Lithuania|              7.94|
++--------+-------------+------------------+
+"""
